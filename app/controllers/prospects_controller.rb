307313eefdbfa275bd44946c00fd1be775e42988
ymf <yesmehfriend@gmail.com>
2015-09-20 12:18:06 +0200

    Add prospect form and creation

--- app/controllers/prospects_controller.rb
+++ app/controllers/prospects_controller.rb
@@ -1,4 +1,21 @@
 class ProspectsController < ApplicationController
   def new
+    @prospect = Prospect.new
   end
+
+  def create
+    @prospect = Prospect.new(prospect_params)
+    if @prospect.save 
+      flash[:success] = "Profile submitted!"
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+    def prospect_params
+      params.require(:prospect).permit(:name, :email, :profile_link, :dev_link)
+    end
 end

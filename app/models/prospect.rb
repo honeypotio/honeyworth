acb721fe604d8250240a3dea72f82fe5c23552b3
YMF <yesmehfriend@gmail.com>
2015-09-21 20:22:37 +0200

    Add error and flash messages + basic validations

--- app/models/prospect.rb
+++ app/models/prospect.rb
@@ -1,2 +1,6 @@
 class Prospect < ActiveRecord::Base
+  before_save { email.downcase! }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+  validates :name, presence: true
 end
